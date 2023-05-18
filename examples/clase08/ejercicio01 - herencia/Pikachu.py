6f8396470d8d5edd2e8b16d0484bfeb043bf9358
Uziel Trujillo <uziel.trujillo@gmail.com>
2023-05-18 12:35:09 -0600

    POO - Clases, Constructor, Herencia 	renamed:    examples/clase07/ejercicio01/Auto.py -> examples/clase07/ejercicio01 - clase/Auto.py 	renamed:    examples/clase07/ejercicio02/Pokemon.py -> examples/clase07/ejercicio02 - constructor/Pokemon.py 	new file:   examples/clase08/ejercicio01 - herencia/Pikachu.py 	new file:   examples/clase08/ejercicio01 - herencia/Pokemon.py 	new file:   examples/clase08/ejercicio01 - herencia/__pycache__/Pikachu.cpython-311.pyc 	new file:   examples/clase08/ejercicio01 - herencia/__pycache__/Pokemon.cpython-311.pyc

---
+++ examples/clase08/ejercicio01 - herencia/Pikachu.py
@@ -0,0 +1,9 @@
+
+from Pokemon import *
+
+class Pikachu(Pokemon):
+    def thunderPunch(self):
+        print('Atacar con thunder punch')
+    
+    def doubleKick(self):
+        print('Atacar con doubleKick')
---
+++ examples/clase08/ejercicio01 - herencia/main.py
@@ -0,0 +1,8 @@
+from Pikachu import *
+
+def run():
+    pikachu = Pikachu(80, 'pikachu', 68, 100)
+    print(pikachu.getDataPokemon())
+
+if __name__ == '__main__':
+    run()
